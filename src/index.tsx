6aa50d4468986b0bab62db8261bce6a11cad9e47
Ilia <6967221@gmail.com>
2023-05-09 22:35:34 +0300

    test macOS

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import App from "./app/App";
 import {BrowserRouter} from "react-router-dom";
 import {ThemeProvider} from "app/providers/ThemeProvider";
 
-
+//test
 render(
     <BrowserRouter>
         <ThemeProvider>
